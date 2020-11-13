b0a8e511b45ef8aae167cf8c21fe2d8cdf045246
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-11-13 13:24:09 -0800

    update: added date and echo cmd

--- bash.js
+++ bash.js
@@ -2,6 +2,7 @@ const { pwd } = require('./pwd');
 const { ls } = require('./ls');
 const { cat } = require('./cat');
 const { curl } = require('./curl');
+const { date } = require('./date');
 
 function done(output) {
   process.stdout.write(output);
@@ -18,13 +19,17 @@ process.stdin.on('data', data => {
       pwd(done);
     } else if (cmd[0] === 'ls') {
       ls(done);
-    } else {
+    } else if (cmd[0] === 'date') {
+      date(done);
+    }else {
       done('You typed: ' + cmd);
     }
   } else if (cmd[0] === 'cat') {
     cat(done, cmd[1]);
   } else if (cmd[0] === 'curl') {
     curl(done, cmd[1]);
+  } else if (cmd[0] === 'echo') {
+    done(cmd.slice(1).join(' '));
   }
 });
 
