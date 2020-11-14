1c064c7db4b7f0ce1f9d6ebab11a82db5b2c85a5
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-11-14 11:25:28 -0800

    refactor: cleaned up curl and date commands

--- curl.js
+++ curl.js
@@ -2,6 +2,7 @@ const request = require('request');
 
 module.exports.curl = function(callback, link) {
   request(link, function(error, response, body) {
+    if (error) throw error;
     callback(body);
   })
 }
